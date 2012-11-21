3524d094fcc74be4d5d5449fe7f57e00ae82c00d
Yohei KANEMARU <yohei-ka@is.naist.jp>
2012-11-21 18:53:38 +0900

    Implement which lock/unlock the MulticastLock of the WiFi.

--- src/jp/naist/inet_lab/android/ipv6multicast/MulticastManager.java
+++ src/jp/naist/inet_lab/android/ipv6multicast/MulticastManager.java
@@ -1,6 +1,7 @@
 package jp.naist.inet_lab.android.ipv6multicast;
 
 import android.content.Context;
+import android.net.wifi.WifiManager;
 
 /**
  * Manage communicating over the IP multicast.
@@ -9,6 +10,12 @@ import android.content.Context;
  * 
  */
 public class MulticastManager {
+
+    /**
+     * A state of a MulticastLock on the WiFi interface
+     */
+    protected WifiManager.MulticastLock multicast_lock;
+
     public MulticastManager() {
 
     }
@@ -63,16 +70,23 @@ public class MulticastManager {
      * 
      * @param context
      *            Context of the application
+     * @param tag
+     *            A tag for identify a state of the WiFi. This tag is only used
+     *            internally.
      */
-    public void enableMulticastOnWifi(Context context) {
-
+    public void enableMulticastOnWifi(Context context, String tag) {
+        WifiManager wifiman = (WifiManager) context
+                .getSystemService(android.content.Context.WIFI_SERVICE);
+        this.multicast_lock = wifiman.createMulticastLock(tag);
+        this.multicast_lock.setReferenceCounted(true);
+        this.multicast_lock.acquire();
     }
 
     /**
      * Disable IP multicast on WiFi interface.
      */
     public void disableMulticastOnWifi() {
-
+        this.multicast_lock.release();
     }
 
 }
