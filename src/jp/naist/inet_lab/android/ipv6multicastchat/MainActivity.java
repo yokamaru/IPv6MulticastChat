4a5673d340a9ebd3cf32c81d09c6e5fea7e7ef13
Yohei KANEMARU <yohei-ka@is.naist.jp>
2012-11-23 20:54:13 +0900

    Add StartActivity on MainActivity to start CharActivity

--- src/jp/naist/inet_lab/android/ipv6multicastchat/MainActivity.java
+++ src/jp/naist/inet_lab/android/ipv6multicastchat/MainActivity.java
@@ -2,19 +2,30 @@ package jp.naist.inet_lab.android.ipv6multicastchat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
     private Button buttonJoin;
+    private EditText editName;
+    private EditText editGroupAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // A form for input the handle-name
+        this.editName = (EditText) this.findViewById(R.id.editName);
+
+        // A form for input the multicast group address
+        this.editGroupAddress = (EditText) this
+                .findViewById(R.id.editGroupAddress);
 
         // A button for join the multicast group
         this.buttonJoin = (Button) this.findViewById(R.id.buttonJoin);
@@ -27,9 +38,15 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 // FIXME: Validate the input(check the name and the address are
                 // valid)
+                String name = editName.getText().toString();
+                String groupAddress = editGroupAddress.getText().toString();
 
                 // Start the ChatAcrivity
-
+                Intent intentToChatActivity = new Intent(
+                        getApplicationContext(), ChatActivity.class);
+                intentToChatActivity.putExtra("name", name);
+                intentToChatActivity.putExtra("group_address", groupAddress);
+                startActivity(intentToChatActivity);
             }
 
         });
