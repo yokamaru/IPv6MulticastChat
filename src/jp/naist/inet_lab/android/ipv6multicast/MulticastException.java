0c996a368e7939142f81d7923eb5b24be66cb6f9
Yohei KANEMARU <yohei-ka@is.naist.jp>
2012-11-23 18:11:35 +0900

    Implement MutlicastException, which handle the exception related to networking

---
+++ src/jp/naist/inet_lab/android/ipv6multicast/MulticastException.java
@@ -0,0 +1,20 @@
+package jp.naist.inet_lab.android.ipv6multicast;
+
+/**
+ * Throw if something issue related to networking is occurred.
+ * 
+ * @author yohei-ka
+ * 
+ */
+public class MulticastException extends Exception {
+
+    /**
+     * Auto-generated serial version UID
+     */
+    private static final long serialVersionUID = -8561020680607603227L;
+
+    public MulticastException(Throwable cause) {
+        super(cause);
+    }
+
+}
