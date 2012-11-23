17eaa819b17c06428afcf4a507bdcd1faf7f8b14
Yohei KANEMARU <yohei-ka@is.naist.jp>
2012-11-23 20:33:11 +0900

    Implement OnClickHandler of buttonJoin on MainActivity

--- src/jp/naist/inet_lab/android/ipv6multicastchat/MainActivity.java
+++ src/jp/naist/inet_lab/android/ipv6multicastchat/MainActivity.java
@@ -3,20 +3,43 @@ package jp.naist.inet_lab.android.ipv6multicastchat;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    private Button buttonJoin;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // A button for join the multicast group
+        this.buttonJoin = (Button) this.findViewById(R.id.buttonJoin);
+        this.buttonJoin.setOnClickListener(new OnClickListener() {
+
+            @Override
+            /*
+             * Start the ChatActivity
+             */
+            public void onClick(View v) {
+                // FIXME: Validate the input(check the name and the address are
+                // valid)
+
+                // Start the ChatAcrivity
+
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
 }
