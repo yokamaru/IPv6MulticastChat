b88f2cbdf4879ab3fb977b6a527b42f41321a12f
Yohei KANEMARU <yohei-ka@is.naist.jp>
2012-11-23 21:26:07 +0900

    Implement joinGroup() and sendMessage() on ChatActivity

--- src/jp/naist/inet_lab/android/ipv6multicastchat/ChatActivity.java
+++ src/jp/naist/inet_lab/android/ipv6multicastchat/ChatActivity.java
@@ -1,15 +1,69 @@
 package jp.naist.inet_lab.android.ipv6multicastchat;
 
+import jp.naist.inet_lab.android.ipv6multicast.MulticastException;
+import jp.naist.inet_lab.android.ipv6multicast.MulticastManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ChatActivity extends Activity {
+
+    /**
+     * Port number which bind to/send to
+     */
+    protected int PORT_NUMBER = 32100;
+
+    /**
+     * A handle-name
+     */
+    protected String name;
+    /**
+     * An address of multicast group
+     */
+    protected String groupAddress;
+
+    /**
+     * Manage communication over multicast
+     */
+    protected MulticastManager multicastManager;
+
+    protected Button buttonSend;
+    protected EditText editMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        editMessage = (EditText) this.findViewById(R.id.editMessage);
+
+        buttonSend = (Button) this.findViewById(R.id.buttonSend);
+        buttonSend.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String message = editMessage.getText().toString();
+                if (!message.isEmpty()) {
+                    message = name + " > " + message;
+                    sendMessage(message);
+                }
+            }
+
+        });
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            this.name = intent.getStringExtra("name");
+            this.groupAddress = intent.getStringExtra("group_address");
+
+            this.joinGroup();
+        }
     }
 
     @Override
@@ -17,6 +71,38 @@ public class ChatActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_chat, menu);
         return true;
+    }
+
+    /**
+     * Join the multicast group
+     */
+    protected void joinGroup() {
+        multicastManager = new MulticastManager();
+
+        try {
+            multicastManager.join(this.groupAddress, this.PORT_NUMBER);
+        } catch (MulticastException e) {
+            Toast.makeText(this, "Faild to join the group.", Toast.LENGTH_LONG)
+                    .show();
+            this.finish();
+        }
+
+    }
+
+    /**
+     * Send the message to the joined multicast group
+     * 
+     * @param message A message to be sent
+     */
+    protected void sendMessage(String message) {
+
+        try {
+            this.multicastManager
+                    .sendData(message.getBytes(), this.PORT_NUMBER);
+        } catch (MulticastException e) {
+            Toast.makeText(this, "Faild to send the group.", Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
 }
